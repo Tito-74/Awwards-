1a25b7142927933f79cf75c780f2e4975ad6a57c
Tito-74 <kipkirui133@gmail.com>
2021-10-24 00:37:54 +0300

    adding models

--- awards/models.py
+++ awards/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    profile_pict = models.ImageField(upload_to = 'images/')
+    bio = models.TextField(max_length = 250, blank=True)
+    name = models.CharField(max_length = 50, blank=True)
+    phone = models.CharField(max_length = 15, blank=True)
+    country = models.CharField(max_length = 50, blank=True)
+
+
+
+class Post(models.Model):
+    user = models.ForeignKey(Profile,on_delete=models.CASCADE, null = True)
+    title = models.CharField(max_length = 100)
+    project_image = models.ImageField(upload_to = 'images/')
+    desc = models.TextField(max_length = 250)
+    url_link = models.UrlField(max_length = 254, blank = True)
+    pub_date = models.DateTimeField(auto_now_add = True)
+
+
+class Review(models.model):
+    user = models.ForeignKey(Profile,on_delete=models.CASCADE, null = True)
+    post = models.ForeignKey(Post,on_delete=models.CASCADE, null = True)
+    design = models.IntegerField(max_length = 10, blank=True)
+    usability = models.IntegerField(max_length = 10, blank=True)
+    creativity = models.IntegerField(max_length = 10, blank=True)
+    comment = models.TextField(max_length = 250)
+    date_rated = models.DateTimeField(auto_now_add = True)
+
+
